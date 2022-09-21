1537fabb75f8f441c25955572c210a7c0a2bbd52
Aiixalex <redcloveralex@gmail.com>
2022-09-21 01:53:08 -0700

    fix bugs

--- daytime/tunnel.c
+++ daytime/tunnel.c
@@ -58,63 +58,61 @@ int main(int argc, char **argv)
 
         if (read(connfd, &msg_from_tunnel, sizeof(msg_from_tunnel)) > 0)
         {
-            break;
+            char serverip[MAXLINE];
+            char serverport[MAXLINE];
+            strcpy(serverip, msg_from_tunnel.serveraddr);
+            strcpy(serverport, msg_from_tunnel.serverport);
+            printf("Server ip: %s\nServer Port: %s\n", msg_from_tunnel.serveraddr, msg_from_tunnel.serverport);
+
+            int    sockfd, s, n;
+            struct addrinfo hints, *result;
+            // struct sockaddr_in servaddr;
+
+            memset(&hints, 0, sizeof(hints));
+            hints.ai_family = AF_INET;
+            hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
+            s = getaddrinfo(serverip, serverport, &hints, &result);
+            if (s != 0) {
+                fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+                exit(EXIT_FAILURE);
+            }
+
+            struct hostent* server;
+            struct sockaddr_in serveraddr;
+            serveraddr = *(struct sockaddr_in*)result->ai_addr;
+            server = gethostbyaddr( (const void *) &serveraddr.sin_addr, sizeof(struct in_addr), AF_INET);
+
+            if ( (sockfd = socket(result->ai_family, result->ai_socktype, 0)) < 0) {
+                printf("socket error\n");
+                exit(EXIT_FAILURE);
+            }
+
+            if (connect(sockfd, result->ai_addr, result->ai_addrlen) < 0) {
+                printf("connect error\n");
+                exit(EXIT_FAILURE);
+            }
+
+            struct message msg;
+            if ( (n = read(sockfd, &msg, sizeof(msg))) > 0) {
+                // recvline[n] = 0;        /* null terminate */
+                if (fprintf(stdout, "Received request from client %s port %d destined to server %s port %s.\n", 
+                            client->h_name, clientport, server->h_name, serverport) == EOF) {
+                    printf("fprintf server name error\n");
+                    exit(EXIT_FAILURE);
+                }
+            }
+            if (n < 0) {
+                printf("read error\n");
+                exit(EXIT_FAILURE);
+            }
+
+            if (write(sockfd, &msg, sizeof(msg)) < 0) {
+                printf("write error\n");
+                exit(EXIT_FAILURE);
+            }
         }
-    }
 
-    close(connfd);
-
-    char serverip[MAXLINE];
-    char serverport[MAXLINE];
-    strcpy(serverip, msg_from_tunnel.serveraddr);
-    strcpy(serverport, msg_from_tunnel.serverport);
-    printf("Server ip: %s\nServer Port: %s\n", msg_from_tunnel.serveraddr, msg_from_tunnel.serverport);
-
-    int    sockfd, s, n;
-    struct addrinfo hints, *result;
-    // struct sockaddr_in servaddr;
-
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
-    s = getaddrinfo(serverip, serverport, &hints, &result);
-    if (s != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
-        exit(EXIT_FAILURE);
-    }
-
-    struct hostent* server;
-    struct sockaddr_in serveraddr;
-    serveraddr = *(struct sockaddr_in*)result->ai_addr;
-    server = gethostbyaddr( (const void *) &serveraddr.sin_addr, sizeof(struct in_addr), AF_INET);
-
-    if ( (sockfd = socket(result->ai_family, result->ai_socktype, 0)) < 0) {
-        printf("socket error\n");
-        exit(EXIT_FAILURE);
-    }
-
-    if (connect(sockfd, result->ai_addr, result->ai_addrlen) < 0) {
-        printf("connect error\n");
-        exit(EXIT_FAILURE);
-    }
-
-    struct message msg;
-    if ( (n = read(sockfd, &msg, sizeof(msg))) > 0) {
-        // recvline[n] = 0;        /* null terminate */
-        if (fprintf(stdout, "Received request from client %s port %d destined to server %s port %s.\n", 
-                    client->h_name, clientport, server->h_name, serverport) == EOF) {
-            printf("fprintf server name error\n");
-            exit(EXIT_FAILURE);
-        }
-    }
-    if (n < 0) {
-        printf("read error\n");
-        exit(EXIT_FAILURE);
-    }
-
-    if (write(sockfd, &msg, sizeof(msg)) < 0) {
-        printf("write error\n");
-        exit(EXIT_FAILURE);
+        close(connfd);
     }
 
     exit(EXIT_SUCCESS);
